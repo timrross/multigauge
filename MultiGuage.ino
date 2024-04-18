05d57aafe5bf435c33271ec2124466e0c63713e7
Tim Ross <timrross@gmail.com>
2024-04-18 10:44:03 +0200

    Upgrade code to use lvgl 9.1

--- MultiGuage.ino
+++ MultiGuage.ino
@@ -49,26 +49,33 @@ Adafruit_BME280 bme;                            // atmosphere pressure/temp modu
 Adafruit_ADS1115 ads;                           // ADC extender module for sensor input
 
 /* Change to your screen resolution */
-static uint32_t screenWidth;
-static uint32_t screenHeight;
-static uint32_t bufSize;
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t *disp_draw_buf;
-static lv_disp_drv_t disp_drv;
+uint32_t screenWidth;
+uint32_t screenHeight;
+uint32_t bufSize;
+lv_display_t *disp;
+lv_color_t *disp_draw_buf;
 
-/* Display flushing */
-void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
-#ifndef DIRECT_MODE
-  uint32_t w = (area->x2 - area->x1 + 1);
-  uint32_t h = (area->y2 - area->y1 + 1);
-
-#if (LV_COLOR_16_SWAP != 0)
-  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
-#else
-  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#if LV_USE_LOG != 0
+  void my_print(lv_log_level_t level, const char *buf) {
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
+  }
 #endif
-#endif  // #ifndef DIRECT_MODE
 
+uint32_t millis_cb(void) {
+  return millis();
+}
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
+  #ifndef DIRECT_MODE
+    uint32_t w = lv_area_get_width(area);
+    uint32_t h = lv_area_get_height(area);
+    gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
+  #endif  // #ifndef DIRECT_MODE
+
+  /*Call it to tell LVGL you are ready*/
   lv_disp_flush_ready(disp);
 }
 
@@ -262,6 +269,10 @@ void readEGTSensor() {
 void setup() {
   Serial.begin(115200);
 
+  #ifdef GFX_EXTRA_PRE_INIT
+    GFX_EXTRA_PRE_INIT();
+  #endif
+
   // Init Display
   if (!gfx->begin()) {
     Serial.println("gfx->begin() failed!");
@@ -275,42 +286,47 @@ void setup() {
 
   lv_init();
 
+  /*Set a tick source so that LVGL will know how much time elapsed. */
+  lv_tick_set_cb(millis_cb);
+
+  /* register print function for debugging */
+  #if LV_USE_LOG != 0
+    lv_log_register_print_cb(my_print);
+  #endif
+
   screenWidth = gfx->width();
   screenHeight = gfx->height();
 
-  bufSize = screenWidth * 40;
-
-
-  #ifdef ESP32
-    disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * bufSize, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-    if (!disp_draw_buf) {
-      // remove MALLOC_CAP_INTERNAL flag try again
-      disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * bufSize, MALLOC_CAP_8BIT);
-    }
+  #ifdef DIRECT_MODE
+    bufSize = screenWidth * screenHeight;
   #else
-    disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * bufSize);
+    bufSize = screenWidth * 40;
   #endif
 
+  #ifdef ESP32
+    #if defined(DIRECT_MODE) && defined(RGB_PANEL)
+      disp_draw_buf = (lv_color_t *)gfx->getFramebuffer();
+    #else   // !DIRECT_MODE
+      disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+      if (!disp_draw_buf) {
+        // remove MALLOC_CAP_INTERNAL flag try again
+        disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_8BIT);
+      }
+    #endif  // !DIRECT_MODE
+  #else   // !ESP32
+    Serial.println("LVGL draw_buf allocate MALLOC_CAP_INTERNAL failed! malloc again...");
+    disp_draw_buf = (lv_color_t *)malloc(bufSize * 2);
+  #endif  // !ESP32
   if (!disp_draw_buf) {
     Serial.println("LVGL disp_draw_buf allocate failed!");
   } else {
-    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, bufSize);
-
-    /* Initialize the display */
-    lv_disp_drv_init(&disp_drv);
-    /* Change the following line to your display resolution */
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.draw_buf = &draw_buf;
-
-    lv_disp_drv_register(&disp_drv);
-
-    /* Initialize the (dummy) input device driver */
-    static lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    lv_indev_drv_register(&indev_drv);
+    disp = lv_display_create(screenWidth, screenHeight);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+    #ifdef DIRECT_MODE
+        lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2, LV_DISPLAY_RENDER_MODE_DIRECT);
+    #else
+        lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2, LV_DISPLAY_RENDER_MODE_PARTIAL);
+    #endif
 
     init_ui();
   }
@@ -390,7 +406,16 @@ void loop() {
   lv_label_set_text_fmt(oil_pressure_label, "%.1f", oil_pressure);
   lv_label_set_text_fmt(egt_label, "%.1f", egt);
   // lv_meter_set_indicator_value(boost_guage, needle, boostPressure);
-  lv_timer_handler(); /* let the GUI do its work */
+
+  lv_task_handler(); /* let the GUI do its work */
+
+  #ifdef DIRECT_MODE
+    #ifdef RGB_PANEL
+      gfx->flush();
+    #else
+      gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)draw_buf, screenWidth, screenHeight);
+    #endif
+  #endif  // #ifdef DIRECT_MODE
   count++;
-  delay(10);
+  delay(5);
 }
